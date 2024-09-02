58ae19d4ad234fee5155a8557b3a27979eba784e
Fernando Chagas <fnd.chagas@gmail.com>
2024-09-02 09:16:37 -0300

    feat - inclui configuracao de testes

--- questoes-facil/typeScript/task01.ts
+++ questoes-facil/typeScript/task01.ts
@@ -1,5 +1,10 @@
-/*  Você está desenvolvendo um sistema de gestão de estoque para uma loja. A loja possui um array de objetos, onde cada objeto representa um produto com propriedades como nome, preço e se está em estoque ou não.*/
-interface Produtos {
+/*  Você está desenvolvendo um sistema de gestão de estoque para uma loja. 
+A loja possui um array de objetos, onde cada objeto representa um produto 
+com propriedades como nome, preço e se está em estoque ou não.*/
+
+// Adicionamos a palavra-chave 'export' à interface Produtos.
+// Isso permite que a interface seja importada em outros arquivos, como nosso arquivo de teste.
+export interface Produtos {
     nome: string;
     preco: number;
     estoque: boolean;
@@ -12,10 +17,13 @@ const produtos: Produtos[] = [
     { nome: "sal", preco: 1.50, estoque: true }
 ];
 
-function produtosEmEstoque(): Produtos[] {
+// Adicionamos a palavra-chave 'export' à função produtosEmEstoque.
+// Isso permite que a função seja importada e testada em outros arquivos.
+// Também modificamos a função para aceitar um parâmetro 'produtos'.
+// Isso torna a função mais flexível e facilita o teste com diferentes conjuntos de dados.
+export function filterProductsInStock(produtos: Produtos[]): Produtos[] {
     return produtos.filter(produto => produto.estoque);
 }
 
-const emEstoque: Produtos[] = produtosEmEstoque();
-
+const emEstoque: Produtos[] = filterProductsInStock(produtos);
 console.log(emEstoque);
